043fc7d6307b6c75b7e9881d80ba9d4d041d1db2
vlashavjeet Jeet <vlashavjeet.singh@powersmiths.com>
2020-03-19 17:16:57 -0400

    creating and sharing project file part is working now

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const port = 3000
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 
 
 app.use('/', express.static('public'))
@@ -16,5 +17,8 @@ app.use('/api', login);
 
 const converter = require('./lib/routes/fileUpload');
 app.use('/api', converter);
+
+const shareFile = require('./lib/routes/shareFile');
+app.use('/api', shareFile);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
